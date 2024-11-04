d7068b5118de37cef89e2c09a3b642ea2c49add8
MarcosAlvesTK <marcos.gama.alves@outlook.com>
2024-11-03 23:00:35 -0300

    commit construtores + listas

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ import br.com.alura.screenmatch.calculos.Recomendacao;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
-        Filme meuFilme = new Filme();
-        meuFilme.setNome("Top Gun");
-        meuFilme.setAnoDeLancamento(1980);
+        Filme meuFilme = new Filme("Top Gun", 2000);
+        //meuFilme.setNome("Top Gun");
         meuFilme.setDuracaoEmMinutos(180);
         System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
 
@@ -27,18 +28,15 @@ public class Main {
         System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
 
-        Serie lost = new Serie();
-        lost.setNome("Lost");
-        lost.setAnoDeLancamento(2000);
+        Serie lost = new Serie("lost", 2000);
         lost.exibirFichaTecnica();
         lost.setTemporadas(10);
         lost.setEpisodiosPorTemporada(10);
         lost.setMinutosPorEpisodio(50);
         System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
 
-        Filme outroFilme = new Filme();
-        outroFilme.setNome("Avatar");
-        outroFilme.setAnoDeLancamento(2023);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        //outroFilme.setNome("Avatar");
         outroFilme.setDuracaoEmMinutos(200);
         System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
 
@@ -51,6 +49,41 @@ public class Main {
 
         Recomendacao filtro = new Recomendacao();
         filtro.filtra(meuFilme);
+
+
+        Filme filmeDoPaulo = new Filme("Dogville", 1970);
+        filmeDoPaulo.setDuracaoEmMinutos(200);
+        //filmeDoPaulo.setNome("Dogville");
+        filmeDoPaulo.avalia(10);
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(filmeDoPaulo);
+        listaDeFilmes.add(meuFilme);
+        listaDeFilmes.add(outroFilme);
+        System.out.println("Tamanho da lista de filme: " + listaDeFilmes.size());
+        System.out.println("Primeiro filme: " + listaDeFilmes.get(0).getNome());
+
+
+        System.out.println(listaDeFilmes);
+        System.out.println(listaDeFilmes.get(0).toString());
+    // É possível escrever dessa forma, pois rescrevemos o método toString da classe objeto (uma superclasse de todas as classes do java) - foi sobrescrito na classe filme
+
+//        for (int i = 0; i < listaDeFilmes.size(); i++) {
+//            System.out.println(listaDeFilmes.get(i).getNome());
+//        }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -6,6 +6,11 @@ public class Filme extends Titulo implements Classificavel {
 
     private String diretor;
 
+    public Filme(String nome, int anoDeLancamento) {
+        super(nome, anoDeLancamento);
+    }
+
+
     public String getDiretor() {
         return diretor;
     }
@@ -18,6 +23,14 @@ public class Filme extends Titulo implements Classificavel {
     public int getClassificao() {
         return (int) pegaMedia()/2; // fazendo CASTING // Convertendo o dado
     }
+
+
+    // sobreescrevendo o método toString, para que ao imprimir um objeto da lista, não saia @ + código hash
+    @Override
+    public String toString() {
+        return "Filme: " + this.getNome() + "(" + this.getAnoDeLancamento() + ")";
+    }
+
 }
 
 
